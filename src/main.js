4bb20c26126b654b5c29e12c9c902c054bd8d022
anujkamboj7 <61876452+anujkamboj7@users.noreply.github.com>
2020-05-05 15:23:41 +0530

    Some changes done in main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
-$(document).ready(function () {
-    $('.menu-icon').on('click', function () {
-        $('.nav-content').toggleClass('active');
-        $('.fa-times').toggleClass('active');
-        $('.overlay').toggleClass('active');
-    })
+$(document).ready(function() {
+	$(".menu-icon").on("click", function() {
+		$(".nav-content").toggleClass("active");
+		$(".fa-times").toggleClass("active");
+		$(".overlay").toggleClass("active");
+	});
+
+	$(".overlay").on("click", function() {
+		$(this).removeClass("active");
+		$(".nav-content").removeClass("active");
+	});
 });
